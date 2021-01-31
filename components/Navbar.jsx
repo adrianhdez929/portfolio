7f85b2753bc65ca00d039b8390739e7f2efaa9b4
adrianhdez929 <adrianhdez929@gmail.com>
2021-01-31 00:47:09 -0500

    Pulished Landing Page

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { createRef, useEffect, useState } from 'react';
 import { Transition } from '@tailwindui/react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
@@ -10,11 +10,9 @@ export default function Navbar(props) {
 
     return(
     <>
-        <nav className="relative w-full flex items-center bg-gray-200 h-12">
+        <nav className="relative w-full flex items-center bg-gray-100 h-16">
             <div className="flex items-center justify-between mx-3 w-full md:w-auto">
-                <div 
-                    className={ router.pathname === '/' ? "text-gray-400 px-5" : "text-gray-700 hover:text-gray-400 px-5" }                    
-                >
+                <div className="text-indigo-700 px-5 text-2xl font-bold">
                     <Link href="/">Portfolio</Link>
                 </div>
                 <div className="md:hidden">
@@ -32,17 +30,17 @@ export default function Navbar(props) {
             </div>         
             <div className="hidden md:flex items-center justify-center px-3 mx-5">
                 <div 
-                    className={ router.pathname === '/projects' ? "text-gray-400 px-5" : "text-gray-700 hover:text-gray-400 px-5" }                    
+                    className={ (router.pathname === '/projects' ? "text-indigo-700" : "text-gray-900 hover:text-indigo-700") + " px-5 text-lg font-bold" }                    
                 >
                     <Link href="/projects">Projects</Link>
                 </div>
                 <div 
-                    className={ router.pathname === '/cv' ? "text-gray-400 px-5" : "text-gray-700 hover:text-gray-400 px-5" }                    
+                    className={ (router.pathname === '/cv' ? "text-indigo-700" : "text-gray-900 hover:text-indigo-700") + " px-5 text-lg font-bold" }                    
                 >
                     <Link href="/cv">Curriculum</Link>
                 </div>
                 <div 
-                    className={ router.pathname === '/blog' ? "text-gray-400 px-5" : "text-gray-700 hover:text-gray-400 px-5" }                    
+                    className={ (router.pathname === '/blog' ? "text-indigo-700" : "text-gray-900 hover:text-indigo-700") + " px-5 text-lg font-bold" }                    
                 >
                     <Link href="/blog">Blog</Link>
                 </div>
@@ -53,12 +51,10 @@ export default function Navbar(props) {
                 <div className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
                     <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
                         <div className="px-5 pt-4 flex items-center justify-between">
-                            <div>
-                                <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="" />
-                            </div>
+                            <div></div>
                             <div className="-mr-2">
                                 <button type="button" className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-                                onClick={ () => setIsOpen(!isOpen) }
+                                    onClick={ () => setIsOpen(!isOpen) }
                                 >
                                 <span className="sr-only">Close main menu</span>
                                 <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
@@ -69,13 +65,13 @@ export default function Navbar(props) {
                         </div>
                         <div role="menu" aria-orientation="vertical" aria-labelledby="main-menu">
                             <div className="px-2 pt-2 pb-3 space-y-1" role="none">
-                                <div className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50" role="menuitem">
+                                <div className="block px-3 py-2 rounded-md text-base font-bold text-gray-700 hover:text-gray-900 hover:bg-gray-50" role="menuitem">
                                     <Link href="/projects">Projects</Link>
                                 </div>
-                                <div className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50" role="menuitem">
+                                <div className="block px-3 py-2 rounded-md text-base font-bold text-gray-700 hover:text-gray-900 hover:bg-gray-50" role="menuitem">
                                     <Link href="/cv">Curriculum</Link>
                                 </div>
-                                <div className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50" role="menuitem">
+                                <div className="block px-3 py-2 rounded-md text-base font-bold text-gray-700 hover:text-gray-900 hover:bg-gray-50" role="menuitem">
                                     <Link href="/blog">Blog</Link>
                                 </div>
                             </div>
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,10 +1,18 @@
+import Head from 'next/head';
+
 import Layout from '../../components/Layout';
 
 
 export default function Projects() {
     return(
         <Layout>
+            <Head>
+                <title>Projects | Adrian Hernandez</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <>
             
+            </>
         </Layout>
     );
 };
