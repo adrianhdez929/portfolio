d1446d52b98c2dd857fcc9baacd4074a326658ef
adrianhdez929 <adrianhdez929@gmail.com>
2021-01-31 21:53:50 -0500

    Added some changes to projects page

--- pages/cv/index.js
+++ pages/cv/index.js
@@ -1,9 +1,17 @@
+import Head from 'next/head';
+
 import Layout from "../../components/Layout";
 
 
 export default function Curriculum() {
     return(
         <Layout>
+            <Head>
+                <title>CV | Adrian Hernandez</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className="w-full h-screen">
+            </div>
         </Layout>
     );
 };
